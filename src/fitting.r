6d81770363182ae32f34d10c7bc2b68005ead240
RokIvansek <ivansek.rok@gmail.com>
2017-05-03 17:37:01 +0200

    added initial values for EM algorithm fitting

--- src/fitting.r
+++ src/fitting.r
@@ -1,8 +1,42 @@
 library(dplyr)
+library(lubridate)
 
-setwd("~/Documents/Faks/Matematika_z_racunalnikom/src")
+setwd("~/Documents/Faks/Matematika_z_racunalnikom/markov_switching_model/src")
 
 load(file='../data/dnevne_cene_2013_2015_deseasonalized.RData')
 
 df <- df %>%
-  select(datum, cena_final_reslm2)
+  select(datum, cena_final_reslm2) %>%
+  rename(cena = cena_final_reslm2)
+
+# Split to train and test
+split_date = mdy("07-01-2015")
+train_data <- df %>% filter(datum < split_date)
+test_data <- df %>% filter(datum >= split_date)
+
+ggplot() + 
+  geom_line(data = train_data, aes(x = datum, y = cena), colour='blue') +
+  geom_line(data = test_data, aes(x = datum, y = cena), colour='red')
+
+
+# We assume the model with normal distributions in both regimes and a bernoulli distribution for govrening the switching
+
+# Initial values for parameters
+set.seed(42)
+
+# Base regime
+mi_base <- 0
+sigma_base <- 8
+
+# Jump regime
+mi_jump <- 0
+sigma_jump <- 20
+
+# Switcing variable
+prob1 <- 0.01 # prob to switch to jump when in base regime
+prob2 <- 0.98 # prob to switch to base when in jump regime
+
+
+
+
+
